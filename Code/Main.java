da4db681b787c2a00c2251ca3b2af0f0167a57a2
aljyu <aljyu@ucsc.edu>
2016-02-15 19:42:11 -0800

    Added code for Main.java and Player.java

---
+++ Code/Main.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args)  {
+        Scanner in = new Scanner (System.in);
+        String input;
+        System.out.println ("Welcome to [the game]!");
+/* Use if you want to do something with the player's name
+        System.out.println ("What is your name?");
+*/
+/*
+        System.out.println ("\nHello " + input + "!");
+*/
+        Player character = new Player (/*input, */100, 100, 5, 0, 30);
+        System.out.println ("You currently have " + character.playerCurrentHP() + " HP and " + character.playerCurrentPotions() + " potions.");
+        System.out.println ("If you wish to view these stats during any point of the game, type in \"Status\".");
+        System.out.println ("\nNow, let's begin!\n");
+        // The beginning of the game
+        
+		
+		
+		// Level 1
+        System.out.println ("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println ("                                                                Level 1");
+        System.out.println ("--------------------------------------------------------------------------------------------------------------------------------------------");
+		character.currentHealth = character.move();
+        // Always call the following 2 lines after every scene
+        input = in.nextLine();
+        status (input, character);
+		character.currentHealth = character.move();
+        // Always call the following 2 lines after every scene
+        input = in.nextLine();
+        status (input, character);
+		
+
+/*
+        // Level 2
+        System.out.println ("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println ("                                                                Level 2");
+        System.out.println ("--------------------------------------------------------------------------------------------------------------------------------------------");
+		// Always call the following 2 lines after every scene
+        input = in.nextLine ();
+        status (input, character);
+
+        // Level 3
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                                                Level 3");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        // Always call the following 2 lines after every scene
+        input = in.nextLine();
+        status(input, character);
+
+        // Level 4
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                                                Level 4");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        // Always call the following 2 lines after every scene
+        input = in.nextLine();
+        status(input, character);
+
+        // Level 5 (Boss Fight)
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                                                Level 5");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+        // Always call the following 2 lines after every scene
+        input = in.nextLine();
+        status(input, character);
+    }
+*/
+
+    // Checks to see if the player types in "status" during any time in the game
+    public static void status(String command, Player person)  {
+        if ((command.equals("Status") || command.equals("status")) && (person.playerCurrentHP() > 0)) {
+            System.out.println("You currently have " + person.playerCurrentHP() + " HP and " + person.playerCurrentPotions() + " potions.\n");
+        }
+    }
+}
--- Code/Player.java
+++ Code/Player.java
@@ -1,33 +1,42 @@
 public class Player{
-	//Player Variabeles
-    private int maxPlayerHealth; // = 100;
-    private String name;
-	private int playerHealth; // = 100;
-	private int playerAttackDamage; //= 50;
-	private int numPotions; // = 3; //starts with 3 potions
-    private int potionHealAmount; //= 30;
-    private int potionDropChance; //= 50 //percent
+    // Player Variables
+//    private String name;
+    public static int maxHealth;
+    public static int currentHealth;
+    public static int attackDamage;
+    public static int numPotions;
+    public static int potionHealAmount;
 
-    // constructor for the player 
-    public Player(String name, int health, int attackDamage, int potions, int heal, int luck){
-    	name = this.name;
-    	playerHealth, maxPlayerHealth = health;
-    	playerAttackDamage = attackDamage;
-    	numPotions = potions;
-    	potionHealAmount = heal;
-    	potionDropChance = luck;
+    // Player's constructor
+    public Player (/*String name, */ int maxHP, int currentHP, int damage, int potion, int heal) {
+//        name = this.name;
+        maxHealth = maxHP;
+        currentHealth = currentHP;
+        attackDamage = damage;
+        numPotions = potion;
+        potionHealAmount = heal;
     }
 
-    /* ------- Funtions for Game Advancement ------ */
-   
-    // get more helth after battleing a bunch
-    public void levelUpHealth ( int newHealth){
-    	playerHealth = newHealth;
+    // Method to view the player's max HP
+    public static int playerMaxHP() {
+        return maxHealth;
     }
-
-
-
-
+    // Method to view the player's current HP
+    public static int playerCurrentHP() {
+        return currentHealth;
     }
-
+    // Method to view the player's current number of potions
+    public static int playerCurrentPotions() {
+        return numPotions;
+    }
+	
+	// Method to state what happens every time the player makes a move
+	public static int move() {
+		return (currentHealth - 5);
+	}
+	
+	// Method to state what happens when the player gets hit (by the boss)
+	public static int hit() {
+		return (currentHealth - 10);
+	}
 }
